5d8d938bf19ce711be9f5adfc00534cb55370319
khusbookhatoon578-gif <khusbookhatoon578@gmail.com>
2026-03-17 21:56:19 +0530

    feature/UC4: Multiple Command-Line Arguments or Default Message

--- HelloApp.java
+++ HelloApp.java
@@ -1,14 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name;
-
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            name = "World";
-        }
+            StringBuilder names = new StringBuilder();
 
-        System.out.println("Hello, " + name + "!");
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+
+                // Add comma except for last name
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
